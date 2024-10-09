048cfad81f9af2084af1c7daeec349e84c23395a
pedrotura <phgt132@gmail.com>
2024-10-08 22:37:40 -0300

    Criando métodos de acesso ao banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/pages/home/home_page.dart';
+import 'package:todo_app/provider/task_group_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: 'https://stooprxaknrtngyhuhth.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0b29wcnhha25ydG5neWh1aHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0MzQ2MzgsImV4cCI6MjA0NDAxMDYzOH0.n8EPSfEChvt47ZZ_wfEaCkLooGuKJttknnzzQAh-uYE',
   );
 
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TaskGroupProvider()..listTaskGroups()),
+      ],
+      child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
